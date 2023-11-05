4fa17282114c19085fdb8d8acc5e4c25be315a71
Carlos V <77943334+valdcarl@users.noreply.github.com>
2023-11-05 16:48:42 -0700

    -Setup initial input for task on form with button -Setup starting point for css for form/input/button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { PlusIcon } from '@heroicons/react/24/solid';
 import "./styles.css";
-
 
 function App() {
   return (
-    <div>
-      HI
+    <div className='container'>
+      <h1 className='app-header'>TODO LIST</h1>
+      <form className='new-task-form'> 
+        <div className='form-row'>
+          <div className='form-input-element'>
+            <label htmlFor='task'></label>
+            <input type='text' id='task' placeholder=" Enter Task Here"/>
+            <button className='add-task-btn' aria-label='Add Task' type='submit'><PlusIcon/>
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
   )
 }
