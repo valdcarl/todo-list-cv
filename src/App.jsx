0e6dbcb670a6d8dc7e4fa870b5b36fca8cd213cd
Carlos Valdez <valdezcarlos.cs@gmail.com>
2023-11-05 21:37:49 -0700

    -Created todo list section in App component -Added styling for this section, come back if need

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
-import { PlusIcon } from '@heroicons/react/24/solid';
+import { PlusIcon, TrashIcon } from '@heroicons/react/24/solid';
 import "./styles.css";
 
 function App() {
   return (
     <div className='container'>
+
       <h1 className='app-header'>TODO LIST</h1>
+
       <form className='new-task-form'> 
         <div className='form-row'>
+
           <div className='form-input-element'>
             <label htmlFor='task'></label>
             <input type='text' id='task' placeholder="Enter Task Here"/>
             <button className='add-task-btn' aria-label='Add Task' type='submit'><PlusIcon/>
             </button>
           </div>
+
         </div>
       </form>
+
+      <ul className='todo-list-section'>
+        <li>
+          <label>
+            <input type='checkbox'/>
+            Item 1
+          </label>
+          <button className='delete-task-btn' aria-label='Delete Task'><TrashIcon/>
+          </button>
+        </li>
+      </ul>
     </div>
   )
 }
