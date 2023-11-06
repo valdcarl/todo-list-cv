f78289d827f3b47574566888d7321f2f1a0e7f69
Carlos Valdez <valdezcarlos.cs@gmail.com>
2023-11-05 21:45:49 -0700

    -Added useState (React State) to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
+import { useState } from 'react';
 import { PlusIcon, TrashIcon } from '@heroicons/react/24/solid';
 import "./styles.css";
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
+  
+
   return (
     <div className='container'>
-
       <h1 className='app-header'>TODO LIST</h1>
-
       <form className='new-task-form'> 
         <div className='form-row'>
-
           <div className='form-input-element'>
             <label htmlFor='task'></label>
-            <input type='text' id='task' placeholder="Enter Task Here"/>
+            <input 
+              value={newTodo} 
+              onChange={e => setNewTodo(e.target.value)}
+              type='text' 
+              id='task' 
+              placeholder="Enter Task Here"/>
             <button className='add-task-btn' aria-label='Add Task' type='submit'><PlusIcon/>
             </button>
           </div>
-
         </div>
       </form>
 
@@ -24,7 +29,7 @@ function App() {
         <li>
           <label>
             <input type='checkbox'/>
-            Item 1
+            Todo 1
           </label>
           <button className='delete-task-btn' aria-label='Delete Task'><TrashIcon/>
           </button>
