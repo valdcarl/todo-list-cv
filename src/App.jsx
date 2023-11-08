17f13aafed1ec4dba8b16e9691a65e37b97204e4
Carlos V <77943334+valdcarl@users.noreply.github.com>
2023-11-08 10:37:35 -0700

    - Added logic for the todos through state/s - Handling a submit in the form for a new task - Upon submission, generating a todo array - a todo in the array is given an id, title, completion boolean - Handling the deletion of a todo task using the id - Handling toggle of completion of a task using id - Handling

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,96 @@ import { PlusIcon, TrashIcon } from '@heroicons/react/24/solid';
 import "./styles.css";
 
 function App() {
-  const [newTodo, setNewTodo] = useState("");
+  //states
+  const [newTask, setNewTask] = useState("");   // for todo input field
+  const [todos, setTodos] = useState([]);       // to create the array of todos
+
+  // submitting todos to our list
+  function handleSubmit(e) {
+    /** handles when a new todo is submitted from out form input 
+     *  we want our app to rerender with this change to take effect
+    */
+    // prevent the
+    e.preventDefault();
+
+    // I know I cant modify todos, so I'm using the spread operator to get a new array
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos, 
+        { id: crypto.randomUUID(), title: newTask, completed: false },
+      ]
+    })
+
+    setNewTask("");   // so that the new todo is cleared after submission
+  }
   
+  // for toggling completion of a todo
+  function toggleTodo(id, completed) {
+    /** takes id and if that id is checked to be marked as completed */
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+
+          // creating a NEW TODO, with a changed property ON IT
+          // the onChange is being called when this being changed, and the approp id for the correct
+          // todo is being toggled
+          return { ...todo, completed }
+        }
+        return todo
+      })
+    })
+  }
+
+  // deleting a todo from out list
+  function deleteTodo(id) {
+    /** deletes a todo based on that todo's id onClick*/
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
+
+  }
 
   return (
     <div className='container'>
       <h1 className='app-header'>
         TODO LIST
       </h1>
-      <form className='new-task-form'> 
+      <form onSubmit={handleSubmit} className='new-task-form'> 
         <div className='form-row'>
           <div className='form-input-element'>
             <label htmlFor='task'></label>
             <input 
-              value={newTodo} 
-              onChange={e => setNewTodo(e.target.value)}
+              value={newTask} 
+              onChange={e => setNewTask(e.target.value)}
               type='text' 
               id='task' 
               placeholder="Enter Task Here"/>
-            <button className='add-task-btn' aria-label='Add Task' type='submit'><PlusIcon/>
-            </button>
+            <button className='add-task-btn' aria-label='Add Task' type='submit'><PlusIcon/> </button>
           </div>
         </div>
       </form>
 
       <ul className='todo-list-section'>
-        <li>
-          <label>
-            <input type='checkbox'/>
-            Todo 1
-          </label>
-          <button className='delete-task-btn' aria-label='Delete Task'><TrashIcon/>
-          </button>
-        </li>
+        {todos.length === 0 && "Nothing For Now!"}
+        {todos.map(todo => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input 
+                  type='checkbox' 
+                  checked={todo.completed}
+                  onChange={e => toggleTodo(todo.id, e.target.checked)} 
+                />
+                {todo.title}
+              </label>
+              <button 
+                onClick={() => deleteTodo(todo.id)}
+                className='delete-task-btn' 
+                aria-label='Delete Task'><TrashIcon/> 
+              </button>
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
